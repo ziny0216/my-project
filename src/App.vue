231344efdd96138ab2f864bcef6b61e316bc5cc6
zinyyy <zinyyy@kacelab.com>
2023-01-02 18:43:05 +0900

    사이드 메뉴 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+ <div class="flex h-screen">
+  <!-- logo-->
+  <div class="w-1/4 mt-5 ml-20"><i class="fab fa-twitter text-2xl text-primary"></i>
+    <!-- icons-->
+  <div class="flex flex-col items-start mt-4">
+    <ul>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="fas fa-fw fa-home "></i> <span class="mr-1">홈</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="fas fa-fw fa-hashtag "></i> <span class="mr-1">탐색하기</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="far fa-fw fa-bell mr-2"></i><span class="mr-1">알림</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="far fa-fw fa-envelope mr-2"></i><span class="mr-1">쪽지</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="far fa-fw fa-bookmark mr-2"></i><span class="mr-1">북마크</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="far fa-fw fa-list-alt  "></i> <span class="mr-1">리스트</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="far fa-fw fa-user "></i> <span class="mr-1">프로필</span></li>
+      <li class="px-4 py-2 text-2xl hover:text-primary hover:bg-blue-50 rounded-full"><i class="fas fa-fw fa-ellipsis-h "></i> <span class="mr-1">더보기</span></li>
+    </ul>
+  </div>
+<!-- button-->
+    <button type="button" class="mt-3 bg-primary text-white w-60 h-12 rounded-full hover:bg-dark">Tweet</button>
+
+  </div>
+
+
+  <div class="flex-1 bg-red-200">dd</div>
+ </div>
 </template>
 
 <script>
